8a2ca5c2ea50b6f1d3f96ef4b226c5ce3ec37aea
briyanyehezkhiel <briyanehezkhiel@gmail.com>
2024-05-19 13:32:51 +0700

    Soal No 4 sudah selesai. Dengan sedikit pembaruan

--- UTS_LAB6_OOP/src/uts_lab6_oop/no4_RumusComplex.java
+++ UTS_LAB6_OOP/src/uts_lab6_oop/no4_RumusComplex.java
@@ -1,84 +1,112 @@
 class Fisika {
-    // Constructor untuk perhitungan Fisika
-    public Fisika() {
-        // Constructor kosong
+    private double massa;
+    private double kecepatan;
+    private double gravitasi;
+    private double tinggi;
+    private double arus;
+    private double resistansi;
+
+    // Constructor untuk inisialisasi dengan parameter massa, kecepatan, gravitasi, tinggi, arus, dan resistansi
+    public Fisika(double massa, double kecepatan, double gravitasi, double tinggi, double arus, double resistansi) {
+        this.massa = massa;
+        this.kecepatan = kecepatan;
+        this.gravitasi = gravitasi;
+        this.tinggi = tinggi;
+        this.arus = arus;
+        this.resistansi = resistansi;
     }
 
     // Metode overload untuk menghitung energi kinetik: EK = 0.5 * m * v^2
-    public double hitungEnergiKinetik(double massa, double kecepatan) {
+    public double hitungEnergiKinetik() {
         return 0.5 * massa * kecepatan * kecepatan;
     }
 
     // Metode overload untuk menghitung energi potensial gravitasi: EP = m * g * h
-    public double hitungEnergiPotensial(double massa, double gravitasi, double tinggi) {
+    public double hitungEnergiPotensial() {
         return massa * gravitasi * tinggi;
     }
 
     // Metode overload untuk menghitung hukum Ohm: V = I * R
-    public double hitungTegangan(double arus, double resistansi) {
+    public double hitungTegangan() {
         return arus * resistansi;
     }
 }
 
 class Matematika {
-    // Constructor untuk perhitungan Matematika
-    public Matematika() {
-        // Constructor kosong
+    private double panjang;
+    private double lebar;
+    private double jariJari;
+    private double sisi;
+    private double tinggi;
+
+    // Constructor untuk inisialisasi dengan parameter panjang, lebar, jari-jari, sisi, dan tinggi
+    public Matematika(double panjang, double lebar, double jariJari, double sisi, double tinggi) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.jariJari = jariJari;
+        this.sisi = sisi;
+        this.tinggi = tinggi;
     }
 
     // Metode overload untuk menghitung luas persegi panjang: L = p * l
-    public double hitung(double panjang, double lebar) {
+    public double hitung() {
         return panjang * lebar;
     }
 
     // Metode overload untuk menghitung luas lingkaran: L = π * r^2
-    public double hitungLuasLingkaran(double jariJari) {
-        return Math.PI * jariJari * jariJari;
+    public double hitung(boolean isLingkaran) {
+        if (isLingkaran) {
+            return Math.PI * jariJari * jariJari;
+        } else {
+            return 0.0; // Kondisi ini tidak akan dipakai, hanya untuk memenuhi syarat overloading
+        }
     }
 
     // Metode overload untuk menghitung volume kubus: V = s^3
-    public double hitungVolumeKubus(double sisi) {
+    public double hitung(int sisi) {
         return Math.pow(sisi, 3);
     }
 
     // Metode overload untuk menghitung volume silinder: V = π * r^2 * t
-    public double hitungVolumeSilinder(double jariJari, double tinggi) {
+    public double hitung(double jariJari, double tinggi) {
         return Math.PI * Math.pow(jariJari, 2) * tinggi;
     }
 }
 
 public class no4_RumusComplex {
     public static void main(String[] args) {
-        // Membuat objek dari kelas Fisika
-        Fisika fisika = new Fisika();
+        // Membuat objek dari kelas Fisika dengan nilai-nilai yang dibutuhkan
+        Fisika fisika = new Fisika(10, 5, 9.8, 20, 2, 5);
+
         // Menghitung energi kinetik
-        double energiKinetik = fisika.hitungEnergiKinetik(10, 5); // massa = 10 kg, kecepatan = 5 m/s
+        double energiKinetik = fisika.hitungEnergiKinetik();
         System.out.println("Energi Kinetik: " + energiKinetik + " Joule");
 
         // Menghitung energi potensial gravitasi
-        double energiPotensial = fisika.hitungEnergiPotensial(10, 9.8, 20); // massa = 10 kg, gravitasi = 9.8 m/s^2, tinggi = 20 m
+        double energiPotensial = fisika.hitungEnergiPotensial();
         System.out.println("Energi Potensial Gravitasi: " + energiPotensial + " Joule");
 
         // Menghitung tegangan menggunakan hukum Ohm
-        double tegangan = fisika.hitungTegangan(2, 5); // arus = 2 A, resistansi = 5 ohm
+        double tegangan = fisika.hitungTegangan();
         System.out.println("Tegangan: " + tegangan + " Volt");
 
-        // Membuat objek dari kelas Matematika
-        Matematika matematika = new Matematika();
+        // Membuat objek dari kelas Matematika dengan nilai-nilai yang dibutuhkan
+        Matematika matematika = new Matematika(10, 5, 7, 3, 10);
+
         // Menghitung luas persegi panjang
-        double luasPersegiPanjang = matematika.hitung(10, 5); // panjang = 10 m, lebar = 5 m
+        double luasPersegiPanjang = matematika.hitung();
         System.out.println("Luas Persegi Panjang: " + luasPersegiPanjang + " m^2");
 
         // Menghitung luas lingkaran
-        double luasLingkaran = matematika.hitungLuasLingkaran(7); // jari-jari = 7 m
+        double luasLingkaran = matematika.hitung(true);
         System.out.println("Luas Lingkaran: " + luasLingkaran + " m^2");
 
         // Menghitung volume kubus
-        double volumeKubus = matematika.hitungVolumeKubus(3); // sisi = 3 m
+        double volumeKubus = matematika.hitung(3);
         System.out.println("Volume Kubus: " + volumeKubus + " m^3");
 
         // Menghitung volume silinder
-        double volumeSilinder = matematika.hitungVolumeSilinder(3, 10); // jari-jari = 3 m, tinggi = 10 m
+        double volumeSilinder = matematika.hitung(3, 10);
         System.out.println("Volume Silinder: " + volumeSilinder + " m^3");
     }
 }
